c10a49b9c687cf17128a97a04896c3f38296b235
Taopol BERQUER <tberquer@rampar.com>
2020-06-23 15:38:02 +0200

    Ajout de l'ui Auth?

---
+++ database/migrations/2020_06_23_125722_create_shares_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSharesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shares', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('read');
+            $table->boolean('write');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('file_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shares');
+    }
+}
