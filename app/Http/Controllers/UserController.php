634207258c33dec558ac0b905eb615553d9430c2
tdargent1 <tdargent1@gmail.com>
2020-06-23 17:23:00 +0200

    Un début difficile mais ça va aller : settings user edit sans controle sur formulaire, merci tao

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\UpdateSettingsRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Show the user settings.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function editSettings()
+    {
+        $user = Auth::user();
+
+        return view('settings', compact('user'));
+    }
+
+    public function updateSettings(UpdateSettingsRequest $request)
+    {
+        Auth::user()->update($request->validated());
+
+        return back()->with('status', 'Vos informations ont été modifiées');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/settings', 'UserController@editSettings')->name('editSettings');
+    Route::put('/settings', 'UserController@updateSettings')->name('updateSettings');
+});
+
+
