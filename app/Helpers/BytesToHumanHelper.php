6e835a946df68a505ba4453402742752b956d958
Taopol BERQUER <tberquer@rampar.com>
2020-07-07 16:07:48 +0200

    Ajout d'un fonction qui convertit les bytes.

---
+++ app/Helpers/BytesToHumanHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Helpers;
+
+class BytesToHumanHelper
+{
+    public static function convert(int $bytes)
+    {
+        $units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB'];
+
+        for ($i = 0; $bytes > 1024; $i++) {
+            $bytes /= 1024;
+        }
+
+        return round($bytes, 2) . ' ' . $units[$i];
+    }
+}
